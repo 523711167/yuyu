59e646e31e2ffe71f0082bbbfc4b1136a1dd0f50
Xixi Peng <xixipeng@192.168.18.120>
2025-01-17 18:02:56 +0800

    init

---
+++ src/main/java/com/xiaoxipeng/yuyu/YuyuApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoxipeng.yuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaoxipeng/yuyu/YuyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoxipeng.yuyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
