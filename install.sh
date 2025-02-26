1c7a54837f3ebb3735efe5532a4b042dd25f9e5d
xxpeng <523711167@qq.com>
2025-02-26 21:47:22 +0800

    1

--- install.sh
+++ install.sh
@@ -1,76 +1,45 @@
 #!/bin/bash
 
+# 启用错误时立即退出
 set -e
 
+# 获取脚本所在文件夹目录,先执行cd然后在pwd赋值
+# $0 表示当前目录+文件名称
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/common.sh
 
+# 取消对文件名扩展的限制，允许在脚本中使用 * 等通配符
 set +o noglob
 
-usage=$'Please set hostname and other necessary attributes in harbor.yml first. DO NOT use localhost or 127.0.0.1 for hostname, because Harbor needs to be accessed by external clients.
-Please set --with-trivy if needs enable Trivy in Harbor.
-Please do NOT set --with-chartmuseum, as chartmusuem has been deprecated and removed.
-Please do NOT set --with-notary, as notary has been deprecated and removed.'
 item=0
-
-# clair is deprecated
-with_clair=$false
-# trivy is not enabled by default
-with_trivy=$false
 
 # flag to using docker compose v1 or v2, default would using v1 docker-compose
 DOCKER_COMPOSE=docker-compose
 
-while [ $# -gt 0 ]; do
-        case $1 in
-            --help)
-            note "$usage"
-            exit 0;;
-            --with-trivy)
-            with_trivy=true;;
-            *)
-            note "$usage"
-            exit 1;;
-        esac
-        shift || true
-done
-
+# ${BASH_SOURCE[0]}表示./install.sh
 workdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $workdir
 
+# 测试docker安装环境
 h2 "[Step $item]: checking if docker is installed ..."; let item+=1
 check_docker
 
+# 测试docker-compose安装环境
 h2 "[Step $item]: checking docker-compose is installed ..."; let item+=1
 check_dockercompose
 
+# 解压mysql、nacos、redis容器环境
 # -f 文件测试的条件操作符,文件存在返回true，反之false
-if [ -f harbor*.tar.gz ]
+if [ -f tool*.tar.gz ]
 then
     h2 "[Step $item]: loading Harbor images ..."; let item+=1
     docker load -i ./harbor*.tar.gz
 fi
 echo ""
 
-h2 "[Step $item]: preparing environment ...";  let item+=1
-if [ -n "$host" ]
-then
-    sed "s/^hostname: .*/hostname: $host/g" -i ./harbor.yml
-fi
-
-h2 "[Step $item]: preparing harbor configs ...";  let item+=1
-prepare_para=
-if [ $with_trivy ]
-then
-    prepare_para="${prepare_para} --with-trivy"
-fi
-
-./prepare $prepare_para
-echo ""
-
 if [ -n "$DOCKER_COMPOSE ps -q"  ]
     then
-        note "stopping existing Harbor instance ..." 
+        note "stopping existing Xiaoxipeng instance ..."
         $DOCKER_COMPOSE down -v
 fi
 echo ""
@@ -78,4 +47,29 @@ echo ""
 h2 "[Step $item]: starting Harbor ..."
 $DOCKER_COMPOSE up -d
 
-success $"----Harbor has been installed and started successfully.----"
+success $"----YuYu has been installed and started successfully.----"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
