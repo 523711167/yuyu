20af28366fbb9cc9e507baad94dfe1560b7d3913
Xixi Peng <xixipeng@192.168.18.104>
2025-03-07 19:00:16 +0800

    1

--- src/main/java/com/xiaoxipeng/yuyu/config/DefaultSecurityConfig.java
+++ src/main/java/com/xiaoxipeng/yuyu/config/DefaultSecurityConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -16,27 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class DefaultSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
-//                .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/error/**", "/favicon.ico").permitAll()
-                        .anyRequest().authenticated()
-                );
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requestMatcher -> {
+                    requestMatcher.requestMatchers("/favicon.ico").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
 }
