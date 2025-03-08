9f41913c933204837406ef1eb6e8259952bd90f9
Xixi Peng <xixipeng@192.168.18.104>
2025-03-08 12:27:32 +0800

    `

--- yuyu-auth/src/main/java/com/xiaoxipeng/AuthApplication.java
+++ yuyu-auth/src/main/java/com/xiaoxipeng/AuthApplication.java
@@ -1,13 +1,15 @@
 package com.xiaoxipeng;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
