aa3989debdd895058da68a92b1aef7485ea01aed
xixipeng <523711167@qq.com>
2025-03-11 12:03:06 +0800

    1

--- yuyu-auth/src/main/java/com/xiaoxipeng/config/DefaultSecurityConfig.java
+++ yuyu-auth/src/main/java/com/xiaoxipeng/config/DefaultSecurityConfig.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.xiaoxipeng.util.RsaUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -31,10 +32,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.UUID;
 
 @Configuration
@@ -121,8 +126,8 @@ public class DefaultSecurityConfig {
     }
 
     @Bean
-    public JWKSource<SecurityContext> jwkSource() {
-        KeyPair keyPair = generateRsaKey();
+    public JWKSource<SecurityContext> jwkSource() throws Exception {
+        KeyPair keyPair = RsaUtils.loadKeyPair("rsa/private.pem", "rsa/public.pem", "RSA");
         RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
         RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
         RSAKey rsaKey = new RSAKey.Builder(publicKey)
@@ -133,18 +138,7 @@ public class DefaultSecurityConfig {
         return new ImmutableJWKSet<>(jwkSet);
     }
 
-    private static KeyPair generateRsaKey() {
-        KeyPair keyPair;
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            keyPair = keyPairGenerator.generateKeyPair();
-        }
-        catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return keyPair;
-    }
+
 
     @Bean
     public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
---
+++ yuyu-auth/src/main/java/com/xiaoxipeng/util/RsaUtils.java
@@ -0,0 +1,59 @@
+package com.xiaoxipeng.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Slf4j
+public class RsaUtils {
+
+    private static PrivateKey loadPrivateKey(String path, String algorithm) throws Exception {
+        try (InputStream is = RsaUtils.class.getClassLoader().getResourceAsStream(path)) {
+            assert is != null;
+            String privateKeyPEM = new String(is.readAllBytes())
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
+            KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+            return keyFactory.generatePrivate(keySpec);
+        }
+    }
+
+    private static PublicKey loadPublicKey(String path, String algorithm) throws Exception {
+        try (InputStream is = RsaUtils.class.getClassLoader().getResourceAsStream(path)) {
+            assert is != null;
+            String publicKeyPEM = new String(is.readAllBytes())
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
+            KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+            return keyFactory.generatePublic(keySpec);
+        }
+    }
+
+    public static KeyPair loadKeyPair(String privateKeyPath, String publicKeyPath, String algorithm) throws Exception {
+        log.info("----------- Load private key from {} -----------", privateKeyPath);
+        // 读取私钥
+        PrivateKey privateKey = loadPrivateKey(privateKeyPath, algorithm);
+
+        log.info("----------- Load public key from {}  -----------", publicKeyPath);
+        // 读取公钥
+        PublicKey publicKey = loadPublicKey(publicKeyPath, algorithm);
+
+        // 创建KeyPair对象
+        KeyPair keyPair = new KeyPair(publicKey, privateKey);
+        log.info("----------- Load RSA key success -----------");
+        return keyPair;
+    }
+
+}
